9f3989d1395e081c8d5b73de89cf96adc2dacda3
Administrator <cold279334079@gmail.com>
2015-11-24 14:02:55 +0800

    ijk调试

---
+++ app/src/main/java/com/freedomplayer/android/tt.java
@@ -0,0 +1,15 @@
+package com.freedomplayer.android;/**
+ * Created by Administrator on 2015/11/23.
+ */
+
+/**
+ * @author GuanXinHua
+ * @version V1.0
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}(用一句话描述该文件做什么)
+ * @email cold279334079@gmail.com
+ * @date ${date} ${time}
+ */
+public class tt {
+}
